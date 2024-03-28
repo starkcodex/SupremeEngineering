b639f27e6eaebc3318d2c8657c87048b0437d431
starkcodex <maneeshemn@gmail.com>
2024-03-28 18:53:02 +0530

    first commit to initaialize vue frontend with routing configurations in application.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
+
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+      
+    <nav class="navbar is-light">
+        
+      <div class="navbar-brand">
+          <router-link to="/" class="navbar-item"><strong>SupremeEng</strong></router-link>
+      </div>
+
+        <div class="navbar-menu">
+          <div class="navbar-end">
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/dashboard" class="navbar-item">Dashboard</router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/home" class="navbar-item">Home</router-link>
+
+                <div class="navbar-item">
+                  <div class="buttons">
+                    <router-link to="/sign-up" class="button is-warning"> <strong>Sign up</strong></router-link>
+                    <router-link to="/log-in" class="button is-light"> <strong>Log in</strong></router-link>
+                  </div>
+                </div>
+
+              </template>
+          </div>
+        </div>
+      </nav>
+  
+    <section class="section">
+      <router-view/>
+    </section>
+
+
+    <footer class="footer">
+        <p class="has-text-centered">Copyright (c) 2024 Inc</p>
+    </footer>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+<script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+import axios from 'axios';
 
-    &.router-link-exact-active {
-      color: #42b983;
+
+  export default {
+    name: 'App',
+    beforeCreate() {
+      this.$store.commit('initializeStore')
+      const token  = this.$store.state.token
+
+      if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+      } else {
+        axios.defaults.headers.common['Authorization'] = ""
+      }
+
     }
   }
-}
+</script>
+
+
+
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Dashboard from '@/views/dashboard/Dashboard.vue'
+import LogIn from '@/views/LogIn.vue'
+import SignUp from '@/views/SignUp.vue'
+
+
+import store from '../store'
+
 
 const routes = [
   {
@@ -14,12 +21,42 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      requireLogin: true
+    }
+  },  
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
+})
+
+
+
 
 export default router
